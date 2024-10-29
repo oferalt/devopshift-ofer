0214943deb716b26ce02bbd05b6781d5ea62ded3
Your Name <you@example.com>
2024-10-29 13:51:36 +0000

    lab1

---
+++ code/azure/solutions/labs/lab1/main.tf
@@ -0,0 +1,99 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "location" {
+  default = "East US"
+}
+variable "myname" {
+  default = "ofer"
+}
+
+variable "vm_size" {
+  default = "Standard_B1ms"
+}
+
+variable "admin_username" {
+  default = "adminuser-[YOURNAME]"
+}
+
+variable "admin_password" {
+  default = "Password123!"
+}
+
+resource "azurerm_resource_group" "rg-ofer" {
+  name     = "${var.myname}-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet-ofer" {
+  name                = "${var.myname}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ofer.name
+}
+
+resource "azurerm_subnet" "subnet-ofer" {
+  name                 = "${var.myname}-subnet"
+  resource_group_name  = azurerm_resource_group.rg-ofer.name
+  virtual_network_name = azurerm_virtual_network.vnet-ofer.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip-ofer" {
+  name                = "${var.myname}-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ofer.name
+  allocation_method   = "Dynamic"  # Dynamic IP allocation for Basic SKU
+  sku = "Basic"  
+}
+
+# Use Basic SKU (Stock Keeping Unit - azure tiers) for dynamic IP
+
+resource "azurerm_network_interface" "nic-ofer" {
+  name                = "${var.myname}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg-ofer.name
+
+  ip_configuration {
+    name                          = "${var.myname}-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet-ofer.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip-ofer.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm-ofer" {
+  name                  = "${var.myname}-vm"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.rg-ofer.name
+  network_interface_ids = [azurerm_network_interface.nic-ofer.id]
+  size                  = var.vm_size
+
+  os_disk {
+    name              = "${var.myname}-os-disk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  computer_name = "${var.myname}-vm"
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip-ofer.ip_address
+  description = "Public IP address of the VM"
+}
+
+
